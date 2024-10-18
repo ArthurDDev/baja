b041c84211e519767e2ae3ff1fedc3717a35a90d
ArthurDDev <ArthurDDev@gmail.com>
2024-10-18 00:07:51 -0300

    [ADD] testes com o google maps

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import  VueGoogleMaps from '@fawmi/vue-google-maps'
+
+const app = createApp(App);
+app.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyBRhpNOTFPfZrG3-r-Vu6XovOIskQoUCaU',
+    },
+}).mount('#app')
